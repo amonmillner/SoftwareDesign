67bd017dec9867e9aa19e424741da4ed8c6255c9
Ryan Eggert <RyanEggertWI@gmail.com>
2014-09-21 12:50:11 -0400

    Beginning QPX tests

--- scraping/QPXExpresstest.py
+++ scraping/QPXExpresstest.py
@@ -1,3 +1,7 @@
 ### QPXExpresstest.py ###
 
 ## Script for experimenting and testing the QPX Express API. This API deals with airfare search and return.
+
+from authenticate import authtokens
+import requests, time
+
