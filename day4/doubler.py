1114c4c1d36ea870f05bf3e5a11f1f135900b2ba
Caleb Kissel <caleb.kissel@students.olin.edu>
2014-09-23 14:12:07 -0400

    commit 2

---
+++ day4/doubler.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 13:51:10 2014
+
+@author: caleb
+"""
+
+def double(x):
+    return 2*x
---
+++ hw2/compare.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 22:18:31 2014
+
+@author: caleb
+"""
+
+def compare(x,y):
+    if x>y:
+        return 1
+    elif x==y:
+        return 0
+    elif x<y:
+        return -1
