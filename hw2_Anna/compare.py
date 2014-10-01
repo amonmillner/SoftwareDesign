3c8f5bb017f4a7a955c6908433b07279f9f53b96
AnnaKnapp <anna.knapp@students.olin.edu>
2014-10-01 02:12:37 -0400

    Added HW

---
+++ hw2_Anna/compare.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 00:12:36 2014
+
+@author: anna
+"""
+def compare(x,y): 
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    elif x < y:
+        return -1
+        
+
+print compare(2,3)
